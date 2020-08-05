89bcb7da93dd0c3cfb383a831e8b0859c559398c
Carolina Ale <redrumjedi@gmail.com>
2020-08-05 00:06:09 -0300

    add TeacherList base structure

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
-
-// import { Container } from './styles';
+import React from "react";
+import { Link } from "react-router-dom";
+import logoImg from "../../assets/images/logo.svg";
+import backIcon from "../../assets/images/icons/back.svg";
 
 function TeacherList() {
-  return <h1>Teachers</h1>
+  return (
+    <div id="page-teacher-list" className="container">
+      <header className="page-header">
+        <div className="top-bar-container">
+          <Link to="/">
+            <img src={backIcon} alt="icone voltar pra home" />
+          </Link>
+          <img src={logoImg} alt="logo proffy" />
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default TeacherList;
