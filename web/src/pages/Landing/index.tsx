46a882062715cba05ca77598943185da9c359f20
Carolina Ale <redrumjedi@gmail.com>
2020-08-04 23:59:32 -0300

    add Link from router-dom to prevent route changes

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom'
 
 import logImg from "../../assets/images/logo.svg";
 import landingImg from "../../assets/images/landing.svg";
@@ -18,14 +19,14 @@ const Landing: React.FC = () => {
         </div>
         <img src={landingImg} alt="plataforma estudos" className="hero-image" />
         <div className="buttons-container">
-          <a href="" className="study">
+          <Link to="study" className="study">
             <img src={studyIcon} alt="study" />
             Estudar
-          </a>
-          <a href="" className="give-classes">
+          </Link>
+          <Link to="give-classes" className="give-classes">
             <img src={giveClassesIcon} alt="give-classes" />
             Dar aulas
-          </a>
+          </Link>
         </div>
         <span className="total-connections">
           Total de 200 conexões já realizadas
