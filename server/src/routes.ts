fb215912ce5cabd7dcee915e638b6a8f473eb24b
Carolina Ale <redrumjedi@gmail.com>
2020-08-06 00:54:04 -0300

    first routes setup

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,8 +2,12 @@ import express from "express";
 
 const routes = express.Router();
 
-routes.get("/", (request, response) => {
-  return response.json({ message: "potato knishes" });
+routes.post("/classes", (request, response) => {
+  const data = request.body;
+
+  console.log(data);
+
+  return response.send();
 });
 
 export default routes;
