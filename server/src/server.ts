ec8e22ec60c6cb6490f6d6f6774408879c228703
Carolina Ale <redrumjedi@gmail.com>
2020-08-06 00:21:50 -0300

    random server file

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,13 @@
-console.log("Hello world")
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (request, response) => {
+  
+    return response.json({ message: 'potato knishes'});
+});
+
+app.listen(3333);
+
