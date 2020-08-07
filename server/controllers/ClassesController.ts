2c6a9aae0bbd50001a95db25360dd15461ebf556
Carolina Ale <redrumjedi@gmail.com>
2020-08-06 23:51:39 -0300

    add filters for available classes w/ user table join

--- server/controllers/ClassesController.ts
+++ server/controllers/ClassesController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 
-import db from "../database/connection";
-import convertHourToMinutes from "../utils/convertHourToMinutes";
+import db from "../src/database/connection";
+import convertHourToMinutes from "../src/utils/convertHourToMinutes";
 
 interface ScheduleItem {
   week_day: number;
@@ -10,6 +10,29 @@ interface ScheduleItem {
 }
 
 export default class ClassesController {
+  async index(request: Request, response: Response) {
+    const filters = request.query;
+
+    const subject = filters.subject as string;
+    const week_day = filters.subject as string;
+    const time = filters.time as string;
+
+    if (!filters.subject || !filters.week_day || !filters.time) {
+      return response.status(400).json({
+        error: "Missing filters",
+      });
+    }
+
+    const timeInMinutes = convertHourToMinutes(time);
+
+    const classes = await db("classes")
+      .where("classes .subject", "=", subject)
+      .join("users", "classes.user_id", "=", "users.id")
+      .select(["classes.*", "users.*"]);
+
+      
+    return response.json(classes);
+  }
   async create(request: Request, response: Response) {
     const {
       name,
